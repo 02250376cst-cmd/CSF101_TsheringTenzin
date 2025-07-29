52a33b7a815a9402f27f37b5d3dd42db6ac2dc16
02250376-cmd <02250376.cst@rub.edu.bt>
2025-07-29 08:45:57 +0000

    my first commit

--- firstprogram.py
+++ firstprogram.py
@@ -1,8 +1,8 @@
 print("hello world")
-print("proko")
-print("hey")
 x = 2
 y = 5
 sum = x+y
-print(sum)
-
+print("The sum of x and y is: ",sum)
+name = input("enter your name: ")
+age = input("enter your age: ")
+print("you are", name, "and you are", age, "years old")
