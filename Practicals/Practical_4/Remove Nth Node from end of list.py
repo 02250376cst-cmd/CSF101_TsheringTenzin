ae304463cde76465907faa02f09af9f410c09971
02250376cst-cmd <02250376.cst@rub.edu.bt>
2025-08-25 15:08:23 +0530

    practical_4

---
+++ Practicals/Practical_4/Remove Nth Node from end of list.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.value = val
+        self.next = next
+def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
+    dummy = ListNode(0)
+    dummy.next = head
+    fast = slow = dummy
+    
+    for _ in range(n):
+       fast = fast.next
+    while fast.next:
+        fast = fast.next
+        slow = slow.next
+    slow.next = slow.next.next
+    return dummy.next
