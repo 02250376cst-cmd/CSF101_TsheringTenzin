d6ea6504a042133474b5124a57c62e36a45d3d10
02250376cst-cmd <02250376.cst@rub.edu.bt>
2025-09-01 20:17:07 +0530

    practical_4

--- firstprogram.py
+++ firstprogram.py
@@ -6,18 +6,3 @@ print("The sum of x and y is: ",sum)
 name = input("enter your name: ")
 age = input("enter your age: ")
 print("you are", name, "and you are", age, "years old")
-
-class LinkedList:
-    # ... (previous code)
-
-    def display(self):
-        elements = []
-        current = self.head
-        while current:
-            elements.append(current.data)
-            current = current.next
-        print(" -> ".join(map(str, elements)))
-
-# Test the display method
-ll = LinkedList()
-ll.display()  # Output: 1 -> 2 -> 3
